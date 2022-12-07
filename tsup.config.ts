c7296b11f1649e9550617d33603df2dd59b95590
wuqinfa <369724377@qq.com>
2022-12-07 18:05:47 +0800

    chore: 增加打包配置

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2022-12-07 17:59:39
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ['cjs', 'esm'],
+});
