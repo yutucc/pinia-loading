bf0b6aecab376d524014f81a1d805d43ab390f66
wuqinfa <369724377@qq.com>
2022-12-09 18:26:10 +0800

    chore: 修改打包方式

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,9 +8,10 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: false,
+  splitting: true,
   sourcemap: false,
   clean: true,
   dts: true,
-  format: ['cjs', 'esm'],
+  format: ['esm'],
+  external: ['pinia']
 });
